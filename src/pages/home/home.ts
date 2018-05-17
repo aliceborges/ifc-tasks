1004dc6e17f4e183319a7d1af5644702824e0fd7
Alice Borges <aliceborges@outlook.com.br>
2018-05-16 22:58:21 -0300

    feat: adição do modal para adicionar tasks

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,19 +8,43 @@ import { Page, Modal, NavController, NavParams, ViewController, Alert, Toast, Ac
 
 export class HomePage {
   static get parameters(){
-    return[[NavController]]
+    return [ [NavController] ];
   }
 
   constructor(nav) {
-    this.navCtrl = nav;
+    this.nav = nav;
 
     this.tasks = [
       {task:'task1', priority:'baixa', body:'content', deadline:'01/01/2019', status:'pendente'},
       {task:'task2', priority:'alta', body:'content', deadline:'01/01/2019', status:'pendente'},
       {task:'task3', priority:'normal', body:'content', deadline:'01/01/2019', status:'pendente'},
       {task:'task4', priority:'alta', body:'content', deadline:'01/01/2019', status:'finalizada'},
-      {task:'task5', priority:'baixa', body:'content', deadline:'01/01/2019', status:'pendente'},
+      {task:'task5', priority:'baixa', body:'content', deadline:'01/01/2019', status:'pendente'}
     ]
   }
 
+  showModal(){
+    let modal = Modal.create(MyModal);
+    this.nav.present(modal);
+  }
+}
+
+@Page({
+  selector: 'page-modal',
+  templateUrl: 'modal.html'
+})
+
+class MyModal{
+  static get parameters(){
+    return [ [NavController], [NavParams], [ViewController] ];
+  }
+
+  constructor(nav, params, viewCtrl){
+    this.nav = nav;
+    this.viewCtrl = viewCtrl;
+  }
+
+  close(){
+    this.viewCtrl.dismiss;
+  }
 }
