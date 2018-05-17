e4b630155bd68dd201c9e525ef2132a0e9490884
Alice Borges <aliceborges@outlook.com.br>
2018-05-16 22:02:28 -0300

    feat: aparência inicial com tasks de teste

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Page, Modal, NavController, NavParams, ViewController, Alert, Toast, ActionSheet } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+  static get parameters(){
+    return[[NavController]]
+  }
 
-  constructor(public navCtrl: NavController) {
+  constructor(nav) {
+    this.navCtrl = nav;
 
+    this.tasks = [
+      {task:'task1', priority:'low', body:'content', deadline:'01/01/2019', status:'pending'},
+      {task:'task2', priority:'high', body:'content', deadline:'01/01/2019', status:'pending'},
+      {task:'task3', priority:'normal', body:'content', deadline:'01/01/2019', status:'pending'},
+      {task:'task4', priority:'high', body:'content', deadline:'01/01/2019', status:'done'},
+      {task:'task5', priority:'low', body:'content', deadline:'01/01/2019', status:'pending'},
+    ]
   }
 
 }
