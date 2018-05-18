a8dbca05d64bda9fd557c279e761c162c761e936
Alice Borges <aliceborges@outlook.com.br>
2018-05-17 22:03:42 -0300

    refactor: tirando comentários do modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, LOCALE_ID } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ModalPageModule } from '../pages/modal/modal.module';
+import { DatabaseProvider } from '../providers/database/database';
+import { TasksProvider } from '../providers/tasks/tasks';
 
 
 @NgModule({
@@ -27,7 +30,11 @@ import { ModalPageModule } from '../pages/modal/modal.module';
   providers: [
     StatusBar,
     SplashScreen,
+    {provide: LOCALE_ID, useValue: 'pt-BR'},
     {provide: ErrorHandler, useClass: IonicErrorHandler},
+    SQLite,
+    DatabaseProvider,
+    TasksProvider
   ]
 })
 export class AppModule {}
--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
-/**
- * Generated class for the ModalPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-modal',
@@ -24,5 +17,5 @@ export class ModalPage {
 
   close(){
     this.viewCtrl.dismiss();
-  } 
+  }
 }
