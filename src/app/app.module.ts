c249972b7f85d35b318bbdeb443757ac84ae6f0d
Alice Borges <aliceborges@outlook.com.br>
2018-05-21 21:38:06 -0300

    refactor: inutilização do sqlite e providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { ErrorHandler, NgModule, LOCALE_ID } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ModalPageModule } from '../pages/modal/modal.module';
-import { DatabaseProvider } from '../providers/database/database';
-import { TasksProvider } from '../providers/tasks/tasks';
-
 
 @NgModule({
   declarations: [
@@ -32,9 +28,6 @@ import { TasksProvider } from '../providers/tasks/tasks';
     SplashScreen,
     {provide: LOCALE_ID, useValue: 'pt-BR'},
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    SQLite,
-    DatabaseProvider,
-    TasksProvider
   ]
 })
 export class AppModule {}
