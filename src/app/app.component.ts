2fe8d7be433955287f260ea3e8b0d98065fba88b
Alice Borges <aliceborges@outlook.com.br>
2018-05-21 22:06:50 -0300

    fix: roteando para a homepage após a exclusão do provider database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,29 +2,19 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { DatabaseProvider } from '../providers/database/database';
 import { HomePage } from '../pages/home/home';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = null; //Retirado a HomePage para não carregar a página sem antes carregar o banco de dados
+  rootPage:any = HomePage; //Retirado a HomePage para não carregar a página sem antes carregar o banco de dados
 
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, dbProvider: DatabaseProvider) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
-
-      dbProvider.createDatabase()
-        .then(() => {this.openHomePage(splashScreen)})
-        .catch((e) => console.error('Um erro aconteceu:', e));
-    });
-  }
-
-  private openHomePage(splashScreen: SplashScreen){
-    splashScreen.hide();
-    this.rootPage = HomePage;
-  }
+  });
+}
 }
